5e4d29e5939e4fe4ebbaa2648b13699d027e12ba
szwiletitsch <szwiletitsch@gmail.com>
2022-09-09 11:52:00 +0200

    added random key generation

--- server_modules/api.js
+++ server_modules/api.js
@@ -1,6 +1,25 @@
+var currentMapData = {}
+
 async function api(request){
     params = request.query
-    return "API NOT IMPLEMENTED"
+
+    if ("newMapForLocation" in params){
+        return await createNewMap(params)
+    }
+    
+    return "THIS API CALL IS INVALID"
+}
+
+async function createNewMap(params){
+    let ort = params["newMapForLocation"]
+    
+    // generates new Keys untill a new one is found 
+    let randomKey;
+    do {
+        randomKey = (Math.random() + 1).toString(36).substring(7);
+    } while (randomKey in currentMapData)
+
+    return randomKey
 }
 
 module.exports = api;
