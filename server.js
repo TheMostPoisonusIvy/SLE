0cc90d3c810796d153e2adfdb0caecc9e9890d5d
szwiletitsch <szwiletitsch@gmail.com>
2022-09-09 10:25:49 +0200

    added file server for website files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const api = require('./server_modules/api')
+const fs = require('fs')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,13 +10,32 @@ app.get('/', async (req, res) => {
   res.sendFile(__dirname + '/website/index.html')
 })
 
-app.get('/map', async (req, res) => {
-  res.sendFile(__dirname + '/website/map.html')
-})
 
 app.get('/api', async (req, res) => {
   let apiResponse = await api(req)
   res.send(apiResponse)
+})
+
+app.get('*', async (req, res) => {
+	let path = req.url.split("/").filter(element => { return element !== ''; });
+	let file = path.pop();
+	file = file.replaceAll(/\?.*$/gm, "")
+
+	try{
+		if (!file.includes(".")){ // if no filetype is specified, set filetype to .html
+			file += ".html";
+		}
+	} catch(e){}
+
+	let directory = "";
+	path.forEach(Element => directory+=Element+"/")
+
+	if (fs.existsSync(__dirname + '/website/' + directory + file)) {
+		res.sendFile(__dirname + '/website/' + directory + file);
+	}
+	else{
+		res.sendFile(__dirname + '/website/' + "404.html")
+	}
 })
 
 app.listen(port, async () => {
