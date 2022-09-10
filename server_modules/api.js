a381b9133e19cb33fe23007b3cb912007cfcbf65
szwiletitsch <szwiletitsch@gmail.com>
2022-09-10 09:35:38 +0200

    added api functions

--- server_modules/api.js
+++ server_modules/api.js
@@ -4,22 +4,39 @@ async function api(request){
     params = request.query
 
     if ("newMapForLocation" in params){
-        return await createNewMap(params)
+        return await createNewMap(params["newMapForLocation"])
+    }
+
+    if ("getMapDataFromKey" in params) {
+        return await getMapData(params["getMapDataFromKey"])
     }
     
     return "THIS API CALL IS INVALID"
 }
 
-async function createNewMap(params){
-    let ort = params["newMapForLocation"]
-    
+async function createNewMap(ort){    
     // generates new Keys untill a new one is found 
     let randomKey;
     do {
         randomKey = (Math.random() + 1).toString(36).substring(7);
     } while (randomKey in currentMapData)
 
+    currentMapData[randomKey] = {"ort": ort, "data": {}}
+
+    console.log(currentMapData)
+
     return randomKey
+}
+
+async function getMapData(key){
+    if (key in currentMapData){
+        return currentMapData[key]
+    }
+    return {}
+}
+
+async function addMapData(request){
+    
 }
 
 module.exports = api;
